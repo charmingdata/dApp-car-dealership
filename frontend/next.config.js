2a7ea53257e64a188c75ef84d700cf83f7469ec8
Maifee Ul Asad <maifeeulasad@gmail.com>
2023-07-24 00:01:38 +0600

    [pipeline + quickfix]: image optimzation+build dir

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: './',
+  assetPrefix: '/',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
